10d3e8397dfbdad290eecb4f040c51b159312f90
unknown <mohammadjavadnoctis2@gmail.com>
2020-06-27 08:19:11 -0700

    Updates

---
+++ precache-manifest.e6dd902140c8396695c1289232a9cd75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86b1557a2cb11587b77863083574f19",
+    "url": "/background-pr/index.html"
+  },
+  {
+    "revision": "c6bc9d823e9476b174a6",
+    "url": "/background-pr/static/css/main.51ec4b81.chunk.css"
+  },
+  {
+    "revision": "6dafb1a71015f14ca9d4",
+    "url": "/background-pr/static/js/2.192f1d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/background-pr/static/js/2.192f1d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bc9d823e9476b174a6",
+    "url": "/background-pr/static/js/main.38964594.chunk.js"
+  },
+  {
+    "revision": "c5fedb172d4b2136b0cf",
+    "url": "/background-pr/static/js/runtime-main.ae50f383.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/background-pr/static/media/logo.5d5d9eef.svg"
+  }
+]);
